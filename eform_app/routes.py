82a3b6d5fc0675dcde530060061f8d281377a64c
AVS1508 <aditya.11.present@gmail.com>
2020-08-29 23:03:41 +0530

    Auth Token Generation

--- eform_app/routes.py
+++ eform_app/routes.py
@@ -1,7 +1,15 @@
+import platform, subprocess, os
 from flask import render_template, request, make_response
 from flask import current_app as app
 from .forms import DonateForm, VolunteerForm
 import pdfkit
+
+def _get_pdfkit_config():
+     if platform.system() == 'Windows':
+         return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
+     else:
+         WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip()
+         return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
 @app.route('/')
 def home():
@@ -18,7 +26,7 @@ def donate():
 @app.route('/pdf_volunteer/', methods=['POST'])
 def pdf_volunteer():
     rendered = render_template('pdf_volunteer.jinja2', data=request.form)
-    pdf = pdfkit.from_string(rendered, False, {'enable-local-file-access': None})
+    pdf = pdfkit.from_string(rendered, False, configuration=_get_pdfkit_config(), options={'enable-local-file-access': None})
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=Volunteering-Form.pdf'
@@ -27,7 +35,7 @@ def pdf_volunteer():
 @app.route('/pdf_donate/', methods=['POST'])
 def pdf_donate():
     rendered = render_template('pdf_donate.jinja2', data=request.form)
-    pdf = pdfkit.from_string(rendered, False, {'enable-local-file-access': None})
+    pdf = pdfkit.from_string(rendered, False, configuration=_get_pdfkit_config(), options={'enable-local-file-access': None})
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=Donation-Form.pdf'
